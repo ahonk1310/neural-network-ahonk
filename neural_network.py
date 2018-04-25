b18eecfc91d11de7d35022e080abfea36fc73f57
Hamzah <hamzah.miftahul@gmail.com>
2018-04-25 19:58:41 +0700

    berak

--- neural_network.py
+++ neural_network.py
@@ -11,6 +11,7 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
+# punyaahong china kampret
 """An Example of a custom Estimator for the Iris dataset."""
 from __future__ import absolute_import
 from __future__ import division
